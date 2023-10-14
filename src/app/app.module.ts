0913a4371ddd763a0957bf5fd54bbd9e41066fcb
Deep Patel <deeppatel7521@gmail.com>
2023-10-14 21:08:48 +0530

    Added all components, song-cards work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SongCardComponent } from './components/song-card/song-card.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    PlaylistComponent,
+    SignupComponent,
+    LoginComponent,
+    SongCardComponent,
+    TopNavComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/song-card/song-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-song-card',
+  templateUrl: './song-card.component.html',
+  styleUrls: ['./song-card.component.css']
+})
+export class SongCardComponent {
+
+  @Input() public playlistThumbnail!: string;
+  @Input() public title!: string;
+  @Input() public description!: string;
+
+}
