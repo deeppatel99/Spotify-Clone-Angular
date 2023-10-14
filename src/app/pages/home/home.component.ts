c0184f2723f0a24d8ce09a30cb56ce2499fc2fd0
Deep Patel <deeppatel7521@gmail.com>
2023-10-14 23:08:19 +0530

    added scroll to horizontal song-cards, login-WIP

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  public songCards = [
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Romantic Songs',
+      description: 'Relax with beautiful tunes'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Workout Songs',
+      description: 'Focus and work hard'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: '80\'s Songs',
+      description: 'Listen to the best music'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Peaceful Songs',
+      description: 'Enjoy the silence'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Mindful Songs',
+      description: 'Listen with your mind'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Workout Songs',
+      description: 'Focus and work hard'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: '80\'s Songs',
+      description: 'Listen to the best music'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Peaceful Songs',
+      description: 'Enjoy the silence'
+    },
+    {
+      thumbnail: 
+      'https://www.seekpng.com/png/full/15-154312_apple-music-logo-png-png-available-on-tidal.png',
+      title: 'Mindful Songs',
+      description: 'Listen with your mind'
+    }
+  ];
 }
